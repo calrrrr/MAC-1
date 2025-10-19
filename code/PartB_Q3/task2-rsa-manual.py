50e646e9d7c79ccec573bca0f8cc5c853b7aa894
Aiza Fontanilla <s4015983@student.rmit.edu.au>
2025-10-19 23:17:58 +1100

    adding comments

--- code/PartB_Q3/task2-rsa-manual.py
+++ code/PartB_Q3/task2-rsa-manual.py
@@ -4,14 +4,17 @@ import math
 # Student Number = s4015983; Message will be 4015983
 message = 4015983
 
+# display message to be decrypted
 print("Message to be decrypted: ", message)
 
-#prime number generator
+# prime number generator
 def prime():
 
     while True:
+        # while it is true, will generate a random number
         num = random.randint(2, 1000)
-            
+        
+        # checks if its a random number
         for i in range(2, int(math.sqrt(num)) + 1):
             if num % i == 0:
                 break
@@ -19,38 +22,40 @@ def prime():
         else:
             return num
 
-#using prime function to generate prime number
+# using prime function to generate prime number
 P = prime()
 Q = prime()
 
-# generating random prime number p and q
+# checking if p and q are the same
 same = True 
 while same:
 
-    # Checking if P and Q are the same
     # P and Q cannot be the same because it breaks the algorithm's security
+    # if p and q is the same, generate a new prime number for q until it is not equal
     if P == Q:
         Q = prime()
     else:
         same = False
-        
+
+# displays keys
 print("Public Key P: ", P)
 print("Publice Key Q: ", Q)
 
-# calculating public key N n : p * q
+# calculating 3rd public key N = p * q
 
 N = P * Q
 
+# prints n
 print("Public Key N: ", N)
 
 # calculating totient
 TOTIENT = (P - 1) * (Q - 1)
 
+# prints tottient
 print("TOTIENT: ", TOTIENT)
 
 # picking prime number e
 # gcd of e and totient = 1 where e is 1<e<totient
-
 def coprimeE(int):
 
     while True:
@@ -61,23 +66,25 @@ def coprimeE(int):
 
 E = coprimeE(TOTIENT)
 
+# prints the random prime selected
 print("random prime: ", E)
 
 # private key generator D using inverse mod modulo
-
 D = pow(E, -1, TOTIENT)
 
+# prints inverse modulo
 print("inverse: ", D)
 
-# Encrption
-
+# Encrption of messahe using formula: m^e mod N
 enc = pow(message, E) % N
 
+# display encyrpted message
 print("encrypted message ", enc)
 
-# decryption
-
+# decryption of cipher using formula C^d mod N
 dec =  pow(enc, D) % N
+
+# display decrypted message 
 print("decrypted message: ", dec)
 
 
